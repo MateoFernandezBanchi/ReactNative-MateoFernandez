38168858ebb80cabbc0dcac00580b6c57ea33a8d
Mateo Fernández Banchi <matefbanchi@gmail.com>
2022-04-22 11:13:08 -0300

    probando

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.Text}>Hola, Coder!</Text>
+      <Text style={styles.Text}>Tienda de vinos online</Text>
+      <TextInput placeholder='Ingrese el vino' style={{color:'#ffff', backgroundColor:'#EBC7C7', padding: 20}} />
+      <Button title='Add' style={styles.Button} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,11 +15,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f2aC',
+    backgroundColor: '#6B0A29',
     alignItems: 'center',
     justifyContent: 'center',
   },
   Text: {
-    fontSize: 36,
+    fontSize: 16,
+    color: '#EBC7C7',
+  }, 
+  Button: {
+    width:100,
+    backgroundColor:'#ffff',
   }
 });
