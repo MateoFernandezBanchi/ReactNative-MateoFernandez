01bd05e8b1ab3b79b3bb6d142bf8522c0188c504
Mateo Fernández Banchi <matefbanchi@gmail.com>
2022-04-25 11:17:08 -0300

    adding image

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.Text}>Tienda de vinos online</Text>
-      <TextInput placeholder='Ingrese el vino' style={{color:'#ffff', backgroundColor:'#EBC7C7', padding: 20}} />
-      <Button title='Add' style={styles.Button} />
+      <Image style={styles.Image} source={{uri:'https://mendozatour.files.wordpress.com/2012/08/gal2.jpg'}} />
+      <View style= {styles.searchContainer}>
+      <TextInput placeholder='Ingrese el vino' style={styles.Input} />
+      <Button title='Buscar' style={styles.Button} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,11 +23,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   Text: {
-    fontSize: 16,
+    fontSize: 20,
     color: '#EBC7C7',
+    paddingBottom:15
   }, 
   Button: {
-    width:100,
     backgroundColor:'#ffff',
+    padding:12,
+  }, 
+  Input: {
+    color:'#ffff', 
+    backgroundColor:'#EBC7C7',
+    padding: 5,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop:20
+  },
+  Image: {
+    width: 200,
+    height:200
   }
 });
